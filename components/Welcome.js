368bb0e7bde542c253eebd92a869ad423939c338
Adrien Lacquemant <github@alaq.io>
2022-02-13 17:25:55 -0500

    Fix prettier build error

--- components/Welcome.js
+++ components/Welcome.js
@@ -3,7 +3,7 @@ export default function SectionContainer() {
     <>
       <h1 className="text-center text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
         Hey, I'm{' '}
-        <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-pink-400">
+        <span className="bg-gradient-to-br·from-blue-400·to-pink-400·bg-clip-text·text-transparent">
           Adrien
         </span>
       </h1>
